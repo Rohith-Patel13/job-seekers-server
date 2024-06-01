56033baea08dd6fa784a8b6e4b22083b1e68ddcd
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-06-01 19:16:18 +0530

    folders research

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const app=express()
 const {connect} = require("mongoose")
 require("dotenv").config()
 const cors=require("cors")
+const morgan=require("morgan")
 app.use(express.json())
 app.use(cors())
+app.use(morgan("dev"))
 
-app.listen(7456,()=>{
+app.listen(7456,async()=>{
     console.log("server running at given port number.")
     try{
-        connect(process.env.MONGODB_URI)
+        await connect(process.env.MONGODB_URI)
         console.log("connected to mongodb database")
     }catch(error){
         console.log(error.message)
     }
 })
+
+//Delete from code after using below url:
+// https://chatgpt.com/c/cdc01684-eddd-4cfc-bd5f-2e8e5d71aa7d
